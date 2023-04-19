78f75691ecc0aabf43d7046879a3bcc883c21365
marviquint <marvsbahio@gmail.com>
2023-04-19 21:58:32 +0800

    added pdf to text conversion

--- scrapy_project/scrapyproject/scrapyproject/spiders/pdfspider.py
+++ scrapy_project/scrapyproject/scrapyproject/spiders/pdfspider.py
@@ -1,6 +1,7 @@
 import scrapy
 import os
-import PyPDF2
+import io
+import pdfplumber
 from django.conf import settings
 
 
@@ -26,27 +27,21 @@ class PDFSpider(scrapy.Spider):
             # Download the PDF for this rule
             pdf_url = data['pdf_link'][0]
             pdf_name = data['rule_name'][0] + '.pdf'
-            file_path = os.path.join('pdfs', pdf_name)
             if not os.path.exists('pdfs'):
                 os.makedirs('pdfs')
-            yield scrapy.Request(url=pdf_url, meta={'file_path': file_path}, callback=self.save_pdf)
+            yield scrapy.Request(url=pdf_url, meta={'pdf_name': pdf_name}, callback=self.save_pdf)
 
     def save_pdf(self, response):
-        # Save the downloaded PDF to the static directory
-        pdf_name = response.url.split('/')[-1]
-        file_path = os.path.join(settings.STATICFILES_DIRS[0], 'pdfs', pdf_name)
-        with open(file_path, 'wb') as f:
-            f.write(response.body)
-        self.logger.info(f'Saved PDF to {file_path}')
-
         # Convert the PDF to a text file
+        pdf_bytes = response.body
+        pdf_name = response.meta['pdf_name']
         text_name = os.path.splitext(pdf_name)[0] + '.txt'
-        text_path = os.path.join(settings.STATICFILES_DIRS[0], 'texts', text_name)
+        text_path = os.path.join(settings.STATICFILES_DIRS[0], 'pdfs', text_name)
         with open(text_path, 'w') as f:
-            pdf_reader = PyPDF2.PdfFileReader(open(file_path, 'rb'))
-            for page_num in range(pdf_reader.getNumPages()):
-                page = pdf_reader.getPage(page_num)
-                text = page.extractText()
-                f.write(text)
+            with io.BytesIO(pdf_bytes) as pdf_file:
+                with pdfplumber.open(pdf_file) as pdf:
+                    for page in pdf.pages:
+                        text = page.extract_text()
+                        f.write(text)
 
         self.logger.info(f'Converted PDF to text file {text_path}')
